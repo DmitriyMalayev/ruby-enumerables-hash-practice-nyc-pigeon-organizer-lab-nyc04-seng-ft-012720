c184a351d27d834bb164016f04ab00166d413964
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-22 01:17:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+
+def nyc_pigeon_organizer(hash)
+new_hash = {}
+hash.each do |data_type, data|
+  data.each do |keys, names|
+    names.each do |name|
+if !new_hash[name]
+  new_hash[name] = {}
+
+
 end
+
+
+  #Will give it truthie or nil
+
+
+binding.pry
+end
+end
+end
+end
+
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
